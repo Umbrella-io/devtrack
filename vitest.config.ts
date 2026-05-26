f69c3d356f7e81864aec3d51cefad86e28fd74fa
Aditya Gautam <aditya.gautam2024@vitstudent.ac.in>
2026-05-26 13:38:21 +0400

    resolve merge conflict

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,24 +5,15 @@ import { dirname, resolve } from "node:path";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-  resolve: {
-    alias: [
-      {
-        find: "@",
-        replacement: resolve(__dirname, "src"),
-      },
-    ],
-import path from "path";
-
-export default defineConfig({
   test: {
     include: ["test/**/*.test.ts"],
     environment: "node",
     globals: true,
   },
+
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": resolve(__dirname, "./src"),
     },
   },
 });
